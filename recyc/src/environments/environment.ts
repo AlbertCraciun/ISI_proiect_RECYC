b2e3cdb885715c36911985e08e8491710ac9845f
AlbertCraciun <73995361+AlbertCraciun@users.noreply.github.com>
2023-12-13 08:20:51 +0200

    added some structure file

---
+++ recyc/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    firebase: {
+      apiKey: "AIzaSyBTG-setzVBe8c-xyc3SMqkRMJqY2stb3g",
+      authDomain: "proiect-isi-recyc.firebaseapp.com",
+      projectId: "proiect-isi-recyc",
+      storageBucket: "proiect-isi-recyc.appspot.com",
+      messagingSenderId: "101532237497",
+      appId: "1:101532237497:web:86ccb559798df28632e568",
+      measurementId: "G-0X9ZF6F8XG"
+    },
+    production: false
+  };
+    
+  // foe development environment
